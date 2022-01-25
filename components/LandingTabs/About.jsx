b24ff562123c342e802e4aea1a1ccbe8e12785ef
Ganning Xu <ganningxu@gmail.com>
2022-01-25 01:22:28 -0500

    update naming

--- components/LandingTabs/About.jsx
+++ components/LandingTabs/About.jsx
@@ -4,7 +4,7 @@ export const About = () => {
     return (
         <Box>
             <Text fontSize='lg'>
-                We are a not-for-profit organization based in Durham, North Carolina with the mission of making the world a better place through the power of language. We are a community of people who love language and want to help make it accessible for everyone.
+                We are an organization based in Durham, North Carolina with the mission of making the world a better place through the power of language. We are a community of people who love language and want to help make it accessible for everyone.
             </Text>
 
         </Box>
