c271b38c9b3c99eb4326b39def831ae88d37a2c8
Ganning Xu <ganningxu@gmail.com>
2022-04-19 21:16:19 -0400

    added about page

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import React, { useState } from "react";
+import { NextSeo } from "next-seo";
+
+export default function Serverless2022() {
+  return (
+    <>
+      <Head>
+        <title>About - Verste</title>
+        <meta
+          httpEquiv="refresh"
+          content="0;URL='https://verste.notion.site/About-Verste-e03cb661bdcb43efa87b41b4cdfd1b0c'"
+        />
+        <meta property="og:site_name" content="verste.org" />
+        <meta property="og:type" content="website" />
+      </Head>
+    </>
+  );
+}
--- pages/contribute.js
+++ pages/contribute.js
@@ -1,42 +1,44 @@
-import { TwoColSignUp } from "../components/Landing/TwoColSignUp"
-import { About } from '../components/LandingTabs/About'
-import { Benefits } from '../components/LandingTabs/Benefits'
-import { Contact } from '../components/LandingTabs/Contact'
-import Head from "next/head"
-import { NextSeo } from 'next-seo';
+import { TwoColSignUp } from "../components/Landing/TwoColSignUp";
+import { About } from "../components/LandingTabs/About";
+import { Benefits } from "../components/LandingTabs/Benefits";
+import { Contact } from "../components/LandingTabs/Contact";
+import Head from "next/head";
+import { NextSeo } from "next-seo";
 
-const url = 'https://verste.org'
-const title = 'Contribute - Verste'
-const description = "Volunteer with us! Verste is an open-source platform where experienced scholars volunteer their time to simplify academic-heavy research in areas ranging from computer science to biology and economics into easy-to-understand versions."
+const url = "https://verste.org";
+const title = "Contribute - Verste";
+const description =
+  "Volunteer with us! Verste is an open-source platform where experienced scholars volunteer their time to simplify academic-heavy research in areas ranging from computer science to biology and economics into easy-to-understand versions.";
 
 export default function Contribute() {
-    return (
-        <>
-            <NextSeo
-                title={title}
-                description={description}
-                canonical={url}
-                openGraph={{
-                    url, title, description
-                }}
-            />
+  return (
+    <>
+      <NextSeo
+        title={title}
+        description={description}
+        canonical={url}
+        openGraph={{
+          url,
+          title,
+          description,
+        }}
+      />
 
-            <TwoColSignUp
-                title1="Make"
-                title2="an"
-                title3="Impact"
-                desc1="Volunteer with us!"
-                desc2="Sign up for early access!"
-                sheet="contribute"
-                pic='/computer.jpeg'
-                tabs={['About', 'Benefits', 'Contact']}
-                tabsContent={[
-                    <About key={1} />,
-                    <Benefits key={2} content='contribute' />,
-                    <Contact key={3} />
-                ]}
-            />
-
-        </>
-    )
+      <TwoColSignUp
+        title1="Make"
+        title2="an"
+        title3="Impact"
+        desc1="Volunteer with us!"
+        desc2="Sign up for early access!"
+        sheet="contribute"
+        pic="/computer.jpeg"
+        tabs={["About", "Benefits", "Contact"]}
+        tabsContent={[
+          <About key={1} />,
+          <Benefits key={2} content="contribute" />,
+          <Contact key={3} />,
+        ]}
+      />
+    </>
+  );
 }
