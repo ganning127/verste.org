2ec61f62c31b88b380449252483e4a55634f0ab0
Ganning Xu <ganningxu@gmail.com>
2022-04-24 01:24:45 -0400

    added redirect for counselors

--- pages/contribute/index.js
+++ pages/contribute/index.js
@@ -7,7 +7,7 @@ export default function Contribute() {
         <title>Contribute - Verste</title>
         <meta
           httpEquiv="refresh"
-          content="0;URL='https://verste.notion.site/Contributors-5fadaf05f79f4cfdac975bcf123ca98c'"
+          content="0;URL='https://verste.notion.site/Verste-Contributors-862f399e67c24f35a3a256904d28d368'"
         />
         <meta property="og:site_name" content="verste.org" />
         <meta property="og:type" content="website" />
