5f734f0d244fb5c403906ed03ffff88a62b83042
Ganning Xu <ganningxu@gmail.com>
2022-06-05 20:24:37 -0400

    added redirect

--- next.config.js
+++ next.config.js
@@ -23,6 +23,12 @@ module.exports = {
           "https://docs.google.com/document/d/1GU1KV7ql5dxkj_dkwGQz6cf1tda92QYGAW4l0rlFj3s/edit?usp=sharing",
         permanent: false,
       },
+      {
+        source: "/contribute/resources",
+        destination:
+          "https://ganning127.github.io/verste-contributor-resources",
+        permanent: false,
+      },
     ];
   },
 };
